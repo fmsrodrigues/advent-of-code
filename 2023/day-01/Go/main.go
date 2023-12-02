81952767113bbed3a201de84c01096e4fc44719b
Phobia <filipe_msr@hotmail.com>
2023-12-02 10:14:18 -0300

    Complete trebuchet second half challenge and refactor code

--- 2023/day-01/Go/main.go
+++ 2023/day-01/Go/main.go
@@ -6,35 +6,45 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"regexp"
 	"strconv"
 	"strings"
 )
 
+var nonNumericalRegex = regexp.MustCompile(`[^0-9 ]+`)
+
+var numbers = map[string]string{
+	"zero":  "z0o",
+	"one":   "o1e",
+	"two":   "t2o",
+	"three": "t3e",
+	"four":  "f4r",
+	"five":  "f5e",
+	"six":   "s6x",
+	"seven": "s7n",
+	"eight": "e8t",
+	"nine":  "n9e",
+}
+
 func main() {
 	file := []string{"documents", "calibration.txt"}
-	calibrationValue := GetCalibrationValue(filepath.Join(file...))
+	path := filepath.Join(file...)
 
+	calibrationValue := GetCalibrationResult(path, GetCalibrationValue)
 	fmt.Printf("Calibration value: %d\n", calibrationValue)
+
+	calibrationMethodWithNumericalStrings := func(s string) int {
+		s = ReplaceNumericalString(s)
+		return GetCalibrationValue(s)
+	}
+	calibrationValueWithNumericalStrings := GetCalibrationResult(path, calibrationMethodWithNumericalStrings)
+	fmt.Printf("Calibration value with numerical strings: %d\n", calibrationValueWithNumericalStrings)
 }
 
-func GetCalibrationValue(filename string) int {
-	documentLines, err := ReadFile(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
+func ReadFile[T any](file string, fn func(string) T) ([]T, error) {
+	var content []T
 
-	var digits []int
-	for _, line := range documentLines {
-		digits = append(digits, GetDigits(line))
-	}
-
-	return SumDigits(digits)
-}
-
-func ReadFile(filename string) ([]string, error) {
-	var lines []string
-
-	f, err := os.Open(filename)
+	f, err := os.Open(file)
 	if err != nil {
 		return nil, err
 	}
@@ -42,39 +52,35 @@ func ReadFile(filename string) ([]string, error) {
 
 	scanner := bufio.NewScanner(f)
 	for scanner.Scan() {
-		lines = append(lines, scanner.Text())
+		content = append(content, fn(scanner.Text()))
 	}
 
 	if err := scanner.Err(); err != nil {
 		return nil, err
 	}
 
-	return lines, nil
+	return content, nil
 }
 
-type Digit struct {
-	Value       int
-	Initialized bool
-}
-
-func GetDigits(s string) int {
-	first := Digit{0, false}
-	last := Digit{0, false}
-
-	characters := strings.Split(s, "")
-
-	for _, c := range characters {
-		if num, err := strconv.Atoi(c); err == nil {
-
-			if !first.Initialized {
-				first = Digit{num, true}
-			}
-
-			last = Digit{num, true}
-		}
+func ReplaceNumericalString(s string) string {
+	for k, v := range numbers {
+		s = strings.ReplaceAll(s, k, v)
 	}
 
-	return (first.Value * 10) + last.Value
+	return s
+}
+
+func GetCalibrationValue(s string) int {
+	s = nonNumericalRegex.ReplaceAllString(s, "")
+
+	rawValue := fmt.Sprintf("%s%s", s[0:1], s[len(s)-1:])
+
+	if num, err := strconv.Atoi(rawValue); err == nil {
+		return num
+	}
+
+	log.Fatalf("Failed to convert %s to int", rawValue)
+	return 0
 }
 
 func SumDigits(d []int) int {
@@ -85,4 +91,13 @@ func SumDigits(d []int) int {
 	}
 
 	return sum
+}
+
+func GetCalibrationResult(s string, calibrationMethod func(string) int) int {
+	calibrationValues, err := ReadFile[int](s, calibrationMethod)
+	if err != nil {
+		log.Fatalf("Failed to read file: %v", err)
+	}
+
+	return SumDigits(calibrationValues)
 }
--- 2023/day-01/Go/main_test.go
+++ 2023/day-01/Go/main_test.go
@@ -6,62 +6,100 @@ import (
 	Trebuchet "trebuchet"
 )
 
-func TestTrebuchet(t *testing.T) {
-	file := []string{"documents", "calibration_test.txt"}
-	cases := []struct {
+type TrebuchetCalibrationTest struct {
+	filepath string
+	cases    []struct {
 		in   string
 		want int
-	}{
-		{"1abc2", 12},
-		{"pqr3stu8vwx", 38},
-		{"a1b2c3d4e5f", 15},
-		{"treb7uchet", 77},
 	}
-	calibrationValue := 142
+	calibrationResult           int
+	withReplaceNumericalStrings bool
+}
 
-	t.Run("Read from file and return all the strings", func(t *testing.T) {
-		filename := filepath.Join(file...)
+func TestTrebuchet(t *testing.T) {
+	calibrationTestWithoutNumericalStrings := TrebuchetCalibrationTest{
+		filepath.Join("documents", "calibration_test_01.txt"),
+		[]struct {
+			in   string
+			want int
+		}{
+			{"1abc2", 12},
+			{"pqr3stu8vwx", 38},
+			{"a1b2c3d4e5f", 15},
+			{"treb7uchet", 77},
+		},
+		142,
+		false,
+	}
 
-		got, err := Trebuchet.ReadFile(filename)
-		if err != nil {
-			t.Errorf("Failed to read file: %v", err)
-		}
+	calibrationTestWithNumericalStrings := TrebuchetCalibrationTest{
+		filepath.Join("documents", "calibration_test_02.txt"),
+		[]struct {
+			in   string
+			want int
+		}{
+			{"two1nine", 29},
+			{"eightwothree", 83},
+			{"abcone2threexyz", 13},
+			{"xtwone3four", 24},
+			{"4nineeightseven2", 42},
+			{"zoneight234", 14},
+			{"7pqrstsixteen", 76},
+		},
+		281,
+		true,
+	}
 
-		for i, c := range cases {
-			if c.in != got[i] {
-				t.Errorf("Trebuchet.ReadFile(%q): got %q, want %q", "calibration_test.txt", got, c.in)
+	for _, calibrationTest := range []TrebuchetCalibrationTest{calibrationTestWithoutNumericalStrings, calibrationTestWithNumericalStrings} {
+		calibrationMethod := func(s string) int {
+			if calibrationTest.withReplaceNumericalStrings {
+				s = Trebuchet.ReplaceNumericalString(s)
 			}
+
+			return Trebuchet.GetCalibrationValue(s)
 		}
-	})
 
-	for _, c := range cases {
-		t.Run("Get the first and last digit in a string and return the combined value", func(t *testing.T) {
-			got := Trebuchet.GetDigits(c.in)
+		t.Run("Read from file and return all the strings", func(t *testing.T) {
+			got, err := Trebuchet.ReadFile[string](calibrationTest.filepath, func(s string) string { return s })
+			if err != nil {
+				t.Errorf("Failed to read file: %v", err)
+			}
 
-			if got != c.want {
-				t.Errorf("Trebuchet.GetDigits(%q): got %d, want %d", c.in, got, c.want)
+			for i, c := range calibrationTest.cases {
+				if c.in != got[i] {
+					t.Errorf("Trebuchet.ReadFile(%q, func(s string) string { return s }): got %q, want %q", calibrationTest.filepath, got, c.in)
+				}
+			}
+		})
+
+		for _, c := range calibrationTest.cases {
+			t.Run("Get the first and last digit in a string and return the combined value", func(t *testing.T) {
+				got := calibrationMethod(c.in)
+
+				if got != c.want {
+					t.Errorf("CalibrationMethod(%q): got %d, want %d", c.in, got, c.want)
+				}
+			})
+		}
+
+		t.Run("Sum the digits returned from the calibration method", func(t *testing.T) {
+			digits := make([]int, len(calibrationTest.cases))
+
+			for _, c := range calibrationTest.cases {
+				digits = append(digits, c.want)
+			}
+
+			got := Trebuchet.SumDigits(digits)
+			if got != calibrationTest.calibrationResult {
+				t.Errorf("Trebuchet.SumDigits(%q): got %d, want %d", digits, got, calibrationTest.calibrationResult)
+			}
+		})
+
+		t.Run("Get the calibration results from the document digits", func(t *testing.T) {
+			got := Trebuchet.GetCalibrationResult(calibrationTest.filepath, calibrationMethod)
+			if got != calibrationTest.calibrationResult {
+				t.Errorf("Trebuchet.GetCalibrationResult(%q, %s): got %d, want %d", calibrationTest.filepath, "calibrationMethod", got, calibrationTest.calibrationResult)
 			}
 		})
 	}
-
-	t.Run("Sum the digits returned from GetDigits", func(t *testing.T) {
-		digits := make([]int, len(cases))
-
-		for _, c := range cases {
-			digits = append(digits, c.want)
-		}
-
-		got := Trebuchet.SumDigits(digits)
-		if got != calibrationValue {
-			t.Errorf("Trebuchet.SumDigits(%q): got %d, want %d", digits, got, calibrationValue)
-		}
-	})
-
-	t.Run("Get the calibration value from document", func(t *testing.T) {
-		got := Trebuchet.GetCalibrationValue(filepath.Join(file...))
-		if got != calibrationValue {
-			t.Errorf("Trebuchet.GetCalibrationValue(%q): got %d, want %d", filepath.Join(file...), got, calibrationValue)
-		}
-	})
-
 }
